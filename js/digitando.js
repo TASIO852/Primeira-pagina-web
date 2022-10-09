cf9b562f5a2f14ae0a5d309b95170a21ab1bf880
Tasio852 <tarsiosantos@outlook.com>
2022-10-08 23:11:16 -0300

    Update

--- js/digitando.js
+++ js/digitando.js
@@ -1,20 +1,19 @@
-
 var content = 'Programando des de sempre !';
 var text = document.getElementById('text');
 
 var speed = 250;
 var cont = 0;
 
-function typeWriter () {
-  if(cont < content.length){
-    text.textContent += content.charAt(cont);
-    cont++;
-    setTimeout(typeWriter, speed);
-  }else{
-    text.textContent = '';
-    cont = 0;
-    typeWriter();
-  }
+function typeWriter() {
+    if (cont < content.length) {
+        text.textContent += content.charAt(cont);
+        cont++;
+        setTimeout(typeWriter, speed);
+    } else {
+        text.textContent = '';
+        cont = 0;
+        typeWriter();
+    }
 }
 
-typeWriter(); 
+typeWriter();
